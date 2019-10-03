ebb94ba04385a59153819dcf1b69d269b2d7d994
Dingyi Fang <df2709@columbia.edu>
2019-10-03 18:50:35 -0400

    update

--- app/server.R
+++ app/server.R
@@ -1,17 +1,17 @@
 library(shiny)
 library(leaflet)
-
 library(data.table)
 library(plotly)
 library(shinyWidgets)
+library(googleVis)
 
 setwd("/Users/jacob/Desktop/FALL 2019/Applied Data Science/fall2019-proj2--sec2-grp8/data")
-activities <- read.csv("activities_processed.csv")
-activities$info <- paste(activities$AGENCY, activities$Contact.Number, activities$Location.1, sep = '\n')
 
+activities <- read.csv("activities_processed.csv")
 kid_activity <- activities[activities$Grade.Level...Age.Group == "Elementary",]
 middle_activity <- activities[activities$Grade.Level...Age.Group == "Middle School",]
 high_activity <- activities[activities$Grade.Level...Age.Group == "High School",]
+
 marker_opt <- markerOptions(opacity = 0.7, riseOnHover = TRUE)
 
 shinyServer(function(input, output,session) {
@@ -20,32 +20,56 @@ shinyServer(function(input, output,session) {
   
   output$map <- renderLeaflet({
     m <- leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite, 
+      addProviderTiles("CartoDB.Positron", 
                        options = providerTileOptions(noWrap = TRUE)) %>%
                         setView(-73.983,40.7639,zoom = 13)
     #plot kids activities
     leafletProxy("map", data = kid_activity) %>%
-      addMarkers(~Longitude, ~Latitude 
-                 # group = "kid_activity" 
-                 , options = marker_opt, label = ~ info ,
+      addMarkers(~Longitude, ~Latitude,
+                 group = "kid_activity" ,
+                 options = marker_opt, popup = ~ paste0("<b>",SITE.NAME,"</b>",
+                                                          "<br/>", "Phone: ", Contact.Number,
+                                                          "<br/>", "Address: ", Location.1, 
+                                                           " ",Postcode) ,
+                 label = ~ SITE.NAME ,
                  icon = list(iconUrl = 'https://cdn2.vectorstock.com/i/1000x1000/47/76/kids-icon-happy-boy-and-girl-children-silhouettes-vector-9674776.jpg'
                              ,iconSize = c(25,25)))
     leafletProxy("map", data = middle_activity) %>%
-      addMarkers(~Longitude, ~Latitude 
-                 # group = "kid_activity" 
-                 , options = marker_opt, label = ~ info,  
+      addMarkers(~Longitude, ~Latitude ,
+                 group = "middle_activity"
+                 , options = marker_opt, popup = ~ paste0("<b>",SITE.NAME,"</b>",
+                                                          "<br/>", "Phone: ", Contact.Number,
+                                                          "<br/>", "Address: ", Location.1, 
+                                                          " ",Postcode) ,  
+                 label = ~ SITE.NAME ,
                  icon = list(iconUrl = 'https://cdn3.iconfinder.com/data/icons/school-pack-3-1/512/5-512.png'
                              ,iconSize = c(25,25)))
     leafletProxy("map", data = high_activity) %>%
-      addMarkers(~Longitude, ~Latitude 
-                 # group = "kid_activity" 
-                 , options = marker_opt, label = ~ info,  
+      addMarkers(~Longitude, ~Latitude,
+                 group = "high_activity"
+                 , options = marker_opt, popup = ~ paste0("<b>",SITE.NAME,"</b>",
+                                                          "<br/>", "Phone: ", Contact.Number,
+                                                          "<br/>", "Address: ", Location.1, 
+                                                          " ",Postcode) ,  
+                 label = ~ SITE.NAME,
                  icon = list(iconUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Closed_Book_Icon.svg/512px-Closed_Book_Icon.svg.png'
                              ,iconSize = c(25,25)))
     m
-    # hide the group not all plot 
-    # leafletProxy("map") %>% hideGroup(c("soemthingouthere"))
   })
+  
+  #enable/disable markers of specific group
+  observeEvent(input$enable_markers, {
+    if("Elementray School" %in% input$enable_markers) leafletProxy("map") %>% showGroup("kid_activity")
+    else{leafletProxy("map") %>% hideGroup("kid_activity")}
+    if("Middle School" %in% input$enable_markers) leafletProxy("map") %>% showGroup("middle_activity")
+    else{leafletProxy("map") %>% hideGroup("middle_activity")}
+    if("High School" %in% input$enable_markers) leafletProxy("map") %>% showGroup("high_activity")
+    else{leafletProxy("map") %>% hideGroup("high_activity")}
+  }, ignoreNULL = FALSE)
+  
+  
+  
+  
   
   ## Statistics Part
   
@@ -56,15 +80,12 @@ shinyServer(function(input, output,session) {
   output$crime_sex <- renderGvis(crime_sex_count)
     
   output$crime_race <- renderGvis(crime_race_count)
-
+  
   # The activities data display column in the Data Search Column  
   output$table1 <- renderDataTable(activities,
-                                   options = list(pageLength = 10, lengthMenu = list(c(10))))
-  
+                                  options = list(pageLength = 10, lengthMenu = list(c(10))))
   # The crime data display column in the Data Search Column  
   output$table2 <- renderDataTable(crime,
                                    options = list(pageLength = 10, lengthMenu = list(c(10))))
-  
-  
 })
 
