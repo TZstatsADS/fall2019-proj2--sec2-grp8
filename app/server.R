d379106fb78df1e4ed7fe40dca3a414ae458d9e9
Dingyi Fang <df2709@columbia.edu>
2019-10-03 13:23:20 -0400

    update

--- app/server.R
+++ app/server.R
@@ -1,13 +1,23 @@
 library(shiny)
+library(leaflet)
+library(data.table)
+library(plotly)
+library(shinyWidgets)
 
 shinyServer(function(input, output) {
-  
+              output$map <- renderLeaflet({
+                              leaflet() %>%
+                              addProviderTiles(providers$Stamen.TonerLite,
+                              options = providerTileOptions(noWrap = TRUE))  %>% 
+                              setView(-73.983,40.7639,zoom = 10) })
   # The activities data display column in the Data Search Column  
-  output$table1 <- renderDataTable(activities,options = list(pageLength = 10, lengthMenu = list(c(10)))
-  )
+  output$table1 <- renderDataTable(activities,
+                                   options = list(pageLength = 10, lengthMenu = list(c(10))))
   
   # The crime data display column in the Data Search Column  
-  output$table2 <- renderDataTable(crime,options = list(pageLength = 10, lengthMenu = list(c(10)))
-  )
+  output$table2 <- renderDataTable(crime,
+                                   options = list(pageLength = 10, lengthMenu = list(c(10))))
+  
+  
 })
 
